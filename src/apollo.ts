3b85ba6626f268e727c64dbb19ecd5ea5b0e024d
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-03-07 17:27:37 -0300

    Buscando dados da Api Graphql

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,9 +1,17 @@
 import ApolloClient from 'apollo-boost'
+import { LAUNCHES_PAST_QUERY } from './graphql/lauches'
 
-const uri = process.env.REAC_APP_API_URL
+const uri = process.env.REACT_APP_API_URL
 
 const client = new ApolloClient({
   uri,
 })
+
+client
+  .query({
+    query: LAUNCHES_PAST_QUERY,
+  })
+  .then(result => console.log('Launches: ', result))
+  .catch(err => console.log('Error: ', err))
 
 export { client }
